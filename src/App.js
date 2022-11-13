e117dab2ebd1b56b1e1e5b8d7cd61a9cdee597a1
felixser1970 <fserrenes@yahoo.es>
2022-11-13 21:53:58 +0100

    version 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from 'react'
+import Tarealist from  './componentes/Tarea'
+import { v4 as uuidv4 } from 'uuid'
+//import Tarea from './componentes/Tarea';
 
-function App() {
+const  LOCAL_STORAGE_KEY = 'tareasApp.tareas'
+
+function App() 
+{
+  const [tareas,setTarea] = useState([]);
+  const tareaNameRef = useRef();
+  // .... almacenaiento del estado
+  useEffect(() =>{
+    const tareasAlmacenadas = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if(tareasAlmacenadas) setTarea(tareasAlmacenadas)
+  },[])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY,JSON.stringify(tareas))
+  },[tareas])
+
+  // funcio que actualiza todas las tareas
+  function accionarTarea(id) {
+    const newTareas = [...tareas]
+    const myTarea = newTareas.find(tarea => tarea.id === id) // ... obtengo referencia al objeto.
+    myTarea.isCompleted = !myTarea.isCompleted               // ... cambio el valor
+    setTarea(newTareas)                                      // ... almaceno todo con los cambios. (renderizar????)
+  }
+  // nuevo nombre de la tarea
+  function nameTarea(id, nombre) {
+    const newTareas = [...tareas]
+    const myTarea = newTareas.find(tarea => tarea.id === id) // ... obtengo referencia al objeto.
+    if(nombre.length) myTarea.name = nombre                  // ... cambio el nombre de cada tarea ....
+    setTarea(newTareas)                                      // ... almaceno todo con los cambios. (renderizar????)
+  }
+
+  function handleAddItem(e) {
+    const name =  tareaNameRef.current.value;
+    if(name === '') return;
+
+    setTarea(prevTareas => { return [...prevTareas, {id: uuidv4(), name: name,isCompleted: false}]}); // añadimos un elemento
+    tareaNameRef.current.value = null;  // anual el valor, lo pone vacío...
+
+  }
+  function handleLimpiarTareas() {
+    const newTareas = tareas.filter(tarea => !tarea.isCompleted)
+    setTarea(newTareas)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1> Lista de Tareas</h1>
+      <Tarealist  tareas={tareas} accionarTarea={accionarTarea} nameTarea={nameTarea}/>
+      <input ref={tareaNameRef} type="text" />
+      <button onClick={handleAddItem}>Agregar Item:</button>
+      <button onClick={handleLimpiarTareas}>Limpiar Completos:</button>
+      <div>{tareas.filter(tarea =>!tarea.isCompleted).length} tareas por hacer</div>
+    </>
   );
 }
 
 export default App;
+
+/*
+ {id: 1, nombre :'tarea 1',isCompleted: false},
+    {id: 2, nombre :'tarea 2',isCompleted: false}, 
+    {id: 3, nombre :'tarea 3',isCompleted: false}
+*/
---
+++ src/componentes/Tarea.js
@@ -0,0 +1,72 @@
+import React, { useState, useRef}  from 'react'
+var ReactDOM = require('react-dom');
+
+
+ // ... componente editar ...
+ // ---------------------------
+function Edicion(props){
+  const tareaNameRef = useRef();
+
+  function cambiarTxt() {
+    props.handleTarName(props.tarea.id, tareaNameRef.current.value)
+  }
+  
+  return (
+    <>
+      <input type="text" ref={tareaNameRef} defaultValue={props.tarea.name}/>
+      <button onClick={cambiarTxt}>Cerrar</button>
+    </>
+  )
+
+}
+
+// .... COMPNENTE TAREA ............
+// ---------------------------------
+function Tarea(props) {
+
+  const [ver,setVer] = useState(false); // Es visible????
+
+  function handleTarea() {
+    setVer(false)
+    props.accionarTarea(props.tareaIndividual.id) // es la función que le se pasa para registrar los cambios, en este caso el 'onchange' defaultChecked={false}
+  }
+
+  // esta función la llama componente ditar, para actualizar las tareas ....
+  function handleTarName(id, nombre) {
+    setVer(false)
+    props.nameTarea(id, nombre) // es la función que le se pasa para registrar los cambios, en este caso el 'onchange' defaultChecked={false}
+  }
+
+  // ... botón EDIT PARA CAMBIAR EL NOMBRE DE LA TAREA.
+  /*function hdEditTarea(id) {
+    const iTar = `${props.tareaIndividual.id}keyk`; // donde se va a insertar 
+    const el = React.createElement('input',{type:'text', value: props.tareaIndividual.name })
+    const myTar = document.getElementById(iTar)
+
+    ReactDOM.render(el,document.getElementById(iTar))
+
+  }*/
+  return (
+    <>
+    <div>
+      <label>
+        <input type="checkbox" id={props.tareaIndividual.id} checked={props.tareaIndividual.isCompleted}  onChange={handleTarea}/>
+        {props.tareaIndividual.name}
+        <button onClick={() => setVer(true)} disabled={ver}>Editar</button>
+      </label>
+      <span>{ver? <Edicion tarea={props.tareaIndividual} handleTarName={handleTarName} /> : '' }</span>
+    </div>
+   </>
+  )  
+}
+
+
+// .... COMPONENTE TAREALIST
+export default function Tarealist( {tareas, accionarTarea, nameTarea} ) {
+  return (
+    tareas.map( tareaIndividual  => {
+      return  <Tarea key={tareaIndividual.id} accionarTarea={accionarTarea} nameTarea={nameTarea} tareaIndividual={tareaIndividual}/>;
+    
+    })
+  )
+}
